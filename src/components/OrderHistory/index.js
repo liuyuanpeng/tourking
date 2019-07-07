6cd7cede9c8991107376700074ee0792622b0bf8
liuyuanpeng <liuyuanpeng@outlook.com>
2019-07-08 01:36:15 +0800

    样式更新

--- src/components/OrderHistory/index.js
+++ src/components/OrderHistory/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Steps, Icon, Modal } from "antd";
+import ORDER_STATUS from '@/pages/Order/orderStatus'
+import moment from 'moment'
 
 const { Step } = Steps;
 export default function OrderHistory({
@@ -23,13 +25,14 @@ export default function OrderHistory({
       <Steps direction="vertical" size="small">
         {data &&
           data.map(item => {
+            const {desc} = ORDER_STATUS.find(orderStatus=>orderStatus.name === item.order_status)
             return (
               <Step
                 key={item.id}
                 status="process"
                 icon={<Icon type="info-circle" />}
-                title={item.title}
-                description={item.desc}
+                title={item.create_time ? moment(item.create_time).format('YYYY-MM-DD HH:mm:ss'): ''}
+                description={`${desc} - ${item.description}`}
               />
             );
           })}
